b94101935c5e056f4f7b7c2627f503d93da3a599
fatimaansemil <114912802+fatimaansemil@users.noreply.github.com>
2025-02-22 23:07:56 +0100

    backend y frontend

---
+++ src/backend/config/auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+    CLIENT_ID: process.env.CLIENT_ID || 'oauth-mkpsbox-oauthlzjswhdwdfzchfeazqsnbxpro',
+    CLIENT_SECRET: process.env.CLIENT_SECRET || 'gpJ[eN67T16{4FTG',
+    TOKEN_URL: process.env.TOKEN_URL || 'https://auth.inditex.com:443/openam/oauth2/itxid/itxidmp/sandbox/access_token',
+};
